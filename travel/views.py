50ce73b5a88153bfe20f892d38380541f9050e84
BLACK <black>
2021-01-13 21:07:18 +0300

    подключил страничку

---
+++ travel/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home_view(request):
+    context = {'name': 'Dave'}
+    return render(request, 'home.html', context)
+
+
+
+
