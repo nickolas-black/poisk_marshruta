4ee70db28b9927ec1bd3924dd0a59805b4a3cdfd
BLACK <black>
2021-01-17 13:34:59 +0300

    добавил отображение информации по городу

--- cities/views.py
+++ cities/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from .models import City
 
 
@@ -7,3 +8,7 @@ def home(request):
     cities = City.objects.all()
     return render(request, 'cities/home.html', {'objects_list': cities})
 
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    context_object_name = 'object'
+    template_name = 'cities/detail.html'
